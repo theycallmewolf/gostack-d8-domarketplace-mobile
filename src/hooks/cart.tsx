62851069003a8286db6a56c2ea135e8f10172126
bruno-wolf <bruno.m.lobato@gmail.com>
2020-12-04 17:27:09 +0000

    loads products from async storage

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -30,7 +30,13 @@ const CartProvider: React.FC = ({ children }) => {
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO LOAD ITEMS FROM ASYNC STORAGE
+      const productsInStorage = await AsyncStorage.getItem(
+        '@GoMarketplace:products',
+      );
+
+      if (productsInStorage) {
+        setProducts([...JSON.parse(productsInStorage)]);
+      }
     }
 
     loadProducts();
